ebdc66d2d29e93d1a074abf8a6cce3b0e0542e44
tdenewiler <tdenewiler@68414a23-2066-41b6-81c2-f5b33d658ffc>
2010-03-29 16:05:03 +0000

    For some reason MikTeX in Windows will not compile this thesis because of an error with the caption package. I don't understand and am not in the mood to track down a bug in how LaTeX compiles under Windows... Will use Linux for compiling for the foreseeable future.
    Several changes to the Lyapunov Matlab code including declaring more variables, making the axes show up better and more comments in the source.
    
    Added a reference to the Aicardi paper on Lyapunov control of unicycle-like vehicles that the Rusu Lyapunov paper was based on.

--- code/lyapunovRusu/CalcETA.m
+++ code/lyapunovRusu/CalcETA.m
@@ -1,5 +1,5 @@
 function [e, theta, alpha] = CalcETA(x, y, phi)
 % Calculate the initial trajectory for the robot.
 e = sqrt(x^2 + y^2);
-theta = atan2(-y, -x);
+theta = atan2(-x, -y);
 alpha = theta - phi;
