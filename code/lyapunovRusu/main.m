926bf707b12f281a4f3291fcbaed37b723d530b6
tdenewiler <tdenewiler@68414a23-2066-41b6-81c2-f5b33d658ffc>
2010-04-10 06:47:01 +0000

    I played around with the Matlab code for the Lyapunov controller a lot tonight. I now have minimum and maximum values for the velocities. The h, k and gamma parameters are being passed from main.m to f3.m so they only have to be changed in one place. I learned that the k parameter really affects the curvature of the robot trajectory, while h cannot be increased too much and gamma will affect both the linear and angular velocities. The comments are also better now.
    Former-commit-id: 5807c619b74797b3979ececfe440b5769e725917
    Former-commit-id: 8ac81a4e2f75975ad2fe8c52e04b6d32073423fc

--- code/lyapunovRusu/main.m
+++ code/lyapunovRusu/main.m
@@ -1,33 +1,51 @@
 % Clear everything and set up plot.
 clc; clf; clear;
+figure(1);
 hold off;
 plot(0, 0);
 hold on;
 
 % Declare variables.
-xstart = -2;
+xstart = 5;
 ystart = 1;
 yawstart = 0;
 xend = 0;
 yend = 0;
-yawend = 0;
+yawend = 3.14;
 step = 0.2;
 time = 20;
+maxu = 5;
+maxw = 5;
 circlesize = 0.02;
-axis([xstart-step step -step ystart+step]);
+if xstart > xend
+    if ystart > yend
+        axis([xend-step xstart+step yend-step ystart+step]);
+    else
+        axis([xend-step xstart+step ystart-step yend+step]);
+    end
+else
+    if ystart > yend
+        axis([xstart-step xend+step yend-step ystart+step]);
+    else
+        axis([xstart-step xend+step ystart-step yend+step]);
+    end
+end
+axis equal;
 
 % Parameters.
+% Note that the gamma, h and k parameters are passed to f3 below as part
+% of y only so that they don't have to be set again in the f3 function
+% and can be modified here only.
 gamma = 1;
-h = 1.1;
-k = 5;
+h = 1;
+k = 6;
 
 % Calculate the initial errors for position and heading.
-[e, th, a] = CalcETA(xstart, ystart, yawstart);
+[e, theta, a] = CalcETA(xstart-xend, ystart-yend, yawstart-yawend);
 
-% Simulate the physics for the trajectory to get the state variables through
-% time. The state variables are y(1) = e, y(2) = alpha and y(3) = theta. This
-% is local planner output between via points based on the Lyapunov function.
-[t, y] = ode45(@f3, [0 time], [e a th]);
+% Simulate the kinematics for the trajectory to get the state variables through
+% time. The state variables are y(1) = e, y(2) = alpha and y(3) = theta.
+[t, y] = ode45(@f3, [0 time], [e a theta gamma h k]);
 
 % Set up vectors to hold x and y positions, yaw speed and velocity of robot.
 xpos = zeros(length(t));
@@ -38,13 +56,17 @@ u = zeros(length(t));
 % At each time step calculate the forward velocity and angular rate commands
 % to issue to the robot.
 for i = 1:length(t)
-    dou = 1;
-    dow = 1;
     xpos(i) = sqrt(y(i,1)^2 * (cos(y(i,3))^2));
     ypos(i) = xpos(i) * tan(y(i,3));
     u(i) = gamma * cos(y(i,2)) * y(i,1);
+    if abs(u(i)) > maxu
+        u(i) = sign(u(i)) * maxu;
+    end
     w(i) = k * y(i,2) + gamma * (cos(y(i,2)) * ...
         sin(y(i,2)) / y(i,2)) * (y(i,2) + h * y(i,3));
+    if abs(w(i)) > maxw
+        w(i) = sign(w(i)) * maxw;
+    end
 end
 
 % Plot the trajectory to go from the start to the goal.
@@ -53,9 +75,14 @@ plot(ypos, xpos, 'r');
 plot(latc, lonc, 'b');
 [latc, lonc] = scircle1(xend, yend, circlesize);
 plot(latc, lonc, 'b');
+hold off;
+
+% Plot x and y position versus time.
+figure(2);
+plot(t, xpos, 'r', t, ypos, 'b-.');
+legend('X', 'Y', 'Location', 'Best');
 
 % Plot the calculated input controls versus time.
-pause;
-hold off;
-plot(t, w, 'r', t, u, 'b');
-legend('w Red','u Blue','Location','Best');
+figure(3);
+plot(t, w, 'r', t, u, 'b-.');
+legend('w', 'u', 'Location', 'Best');
