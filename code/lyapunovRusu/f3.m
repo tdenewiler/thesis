f315beceb5ec0eb286ac71977239a2e2b8f9e791
tdenewiler <tdenewiler@68414a23-2066-41b6-81c2-f5b33d658ffc>
2010-04-10 06:47:01 +0000

    I played around with the Matlab code for the Lyapunov controller a lot tonight. I now have minimum and maximum values for the velocities. The h, k and gamma parameters are being passed from main.m to f3.m so they only have to be changed in one place. I learned that the k parameter really affects the curvature of the robot trajectory, while h cannot be increased too much and gamma will affect both the linear and angular velocities. The comments are also better now.

--- code/lyapunovRusu/f3.m
+++ code/lyapunovRusu/f3.m
@@ -1,10 +1,10 @@
 function dx = f3(t, y)
-% Parameters.
-gamma = 1;
-h = 1;
-k = 6;
 
-% Dynamics of the state variables.
-dx = [-(gamma * cos(y(2))^2 * y(1));
-    -k * y(2) - gamma * h * cos(y(2)) * sin(y(2)) / y(2);
-    gamma * cos(y(2)) * sin(y(2))];
+% Initial conditions.
+dx = [-(y(4) * cos(y(2))^2 * y(1));
+    -y(6) * y(2) - y(4) * y(5) * cos(y(2)) * sin(y(2)) / y(2);
+    y(4) * cos(y(2)) * sin(y(2));
+    0;
+    0;
+    0];
+
