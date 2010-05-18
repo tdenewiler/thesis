d7249daf3a0e4bbf9663b1acd09b809da0485670
tdenewiler <tdenewiler@68414a23-2066-41b6-81c2-f5b33d658ffc>
2010-05-18 00:40:22 +0000

    Added much better explanation to Lyapunov section, including a better image. Cleaned up the Rusu code more and started plotting \dot{V} to make sure that it is negative. That will also be a useful value to calculate in the MPRS_Core code.
    Former-commit-id: 50a2207d9795a87f0198764381289baf35fa5249
    Former-commit-id: 9572f9e26180d5585ca985a3b174dc530272ecb8

--- code/lyapunovRusu/main.m
+++ code/lyapunovRusu/main.m
@@ -6,14 +6,14 @@ plot(0, 0);
 hold on;
 
 % Declare variables.
-xstart = 5;
+xstart = -1;
 ystart = 1;
 yawstart = 0;
 xend = 0;
 yend = 0;
-yawend = 3.14;
+yawend = 0;
 step = 0.2;
-time = 20;
+time = 10;
 maxu = 5;
 maxw = 5;
 circlesize = 0.02;
@@ -43,8 +43,9 @@ k = 6;
 % Calculate the initial errors for position and heading.
 [e, theta, a] = CalcETA(xstart-xend, ystart-yend, yawstart-yawend);
 
-% Simulate the kinematics for the trajectory to get the state variables through
-% time. The state variables are y(1) = e, y(2) = alpha and y(3) = theta.
+% Simulate the kinematics for the trajectory to get the state variables
+% through time. The state variables are:
+% y(1) = e, y(2) = alpha and y(3) = theta.
 [t, y] = ode45(@f3, [0 time], [e a theta gamma h k]);
 
 % Set up vectors to hold x and y positions, yaw speed and velocity of robot.
@@ -52,9 +53,10 @@ xpos = zeros(1,length(t));
 ypos = zeros(1,length(t));
 w = zeros(1,length(t));
 u = zeros(1,length(t));
+Vdot = zeros(1,length(t));
 
 % At each time step calculate the forward velocity and angular rate commands
-% to issue to the robot.
+% to issue to the robot. Note that Vdot should be <= 0 is a sanity check.
 for i = 1:length(t)
     xpos(i) = sqrt(y(i,1)^2 * (cos(y(i,3))^2));
     ypos(i) = xpos(i) * tan(y(i,3));
@@ -67,6 +69,9 @@ for i = 1:length(t)
     if abs(w(i)) > maxw
         w(i) = sign(w(i)) * maxw;
     end
+    Vdot = -e * u * cos(y(i,2)) + y(i,2) * ...
+        (-w(i) + u(i) * sin(y(i,2)) / y(i,2) * ...
+        (y(i,2) + h * theta) / y(i,1));
 end
 
 % Plot the trajectory to go from the start to the goal.
@@ -86,3 +91,9 @@ legend('X', 'Y', 'Location', 'Best');
 figure(3);
 plot(t, w, 'r', t, u, 'b-.');
 legend('w', 'u', 'Location', 'Best');
+
+% Plot the derivative of the Lyapunov function versus time. This should be
+% negative.
+figure(4);
+plot(t, Vdot, 'r');
+legend('Vdot', 'Location', 'Best');
