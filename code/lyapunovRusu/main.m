5007698b54e5328054492b96766f42ff73cb8e2c
tdenewiler <tdenewiler@68414a23-2066-41b6-81c2-f5b33d658ffc>
2010-07-06 16:55:33 +0000

    Adding changes from work to resolve conflicts.
    Former-commit-id: 1d6d2890edfeb0fa9cade74487d69c264c3baab6
    Former-commit-id: c2278bc91a7cfa13779ac913767a51b26fb2ca5b

--- code/lyapunovRusu/main.m
+++ code/lyapunovRusu/main.m
@@ -1,44 +1,26 @@
 % Clear everything and set up plot.
 clc; clf; clear;
-figure(1);
-hold off;
-plot(0, 0);
-hold on;
 
 % Declare variables.
-xstart = -1;
-ystart = 1;
+xstart = 20;
+ystart = -10;
 yawstart = 0;
 xend = 0;
 yend = 0;
 yawend = 0;
 step = 0.2;
-time = 10;
+time = 30;
 maxu = 5;
 maxw = 5;
-circlesize = 0.02;
-if xstart > xend
-    if ystart > yend
-        axis([xend-step xstart+step yend-step ystart+step]);
-    else
-        axis([xend-step xstart+step ystart-step yend+step]);
-    end
-else
-    if ystart > yend
-        axis([xstart-step xend+step yend-step ystart+step]);
-    else
-        axis([xstart-step xend+step ystart-step yend+step]);
-    end
-end
-axis equal;
+borderpercent = 1.1;
 
 % Parameters.
 % Note that the gamma, h and k parameters are passed to f3 below as part
 % of y only so that they don't have to be set again in the f3 function
 % and can be modified here only.
-gamma = 1;
-h = 1;
-k = 6;
+gamma = 0.3;
+h = 0.1;
+k = 1;
 
 % Calculate the initial errors for position and heading.
 [e, theta, a] = CalcETA(xstart-xend, ystart-yend, yawstart-yawend);
@@ -54,6 +36,10 @@ ypos = zeros(1,length(t));
 w = zeros(1,length(t));
 u = zeros(1,length(t));
 Vdot = zeros(1,length(t));
+xposmin = 0;
+xposmax = 0;
+yposmin = 0;
+yposmax = 0;
 
 % At each time step calculate the forward velocity and angular rate commands
 % to issue to the robot. Note that Vdot should be <= 0 is a sanity check.
@@ -72,28 +58,48 @@ for i = 1:length(t)
     Vdot = -y(i,1) * u * cos(y(i,2)) + y(i,2) * ...
         (-w(i) + u(i) * sin(y(i,2)) / y(i,2) * ...
         (y(i,2) + h * theta) / y(i,1));
+
+    % Set up the axes on the plot to show entire trajectory.
+    if xpos(i) > xposmax
+        xposmax = xpos(i);
+    end
+    if xpos(i) < xposmin
+        xposmin = xpos(i);
+    end
+    if ypos(i) > yposmax
+        yposmax = ypos(i);
+    end
+    if ypos(i) < yposmin
+        yposmin = ypos(i);
+    end
 end
 
 % Plot the trajectory to go from the start to the goal.
-plot(ypos, xpos, 'r');
-[latc, lonc] = scircle1(xstart, ystart, circlesize);
-plot(latc, lonc, 'b');
-[latc, lonc] = scircle1(xend, yend, circlesize);
-plot(latc, lonc, 'b');
+figure(1);
+hold on;
+axis([borderpercent * xposmin borderpercent * xposmax borderpercent * yposmin borderpercent * yposmax]);
+axis equal
+plot(xpos, ypos, 'b');
+plot(xstart, ystart, 'go');
+plot(xend, yend, 'ro');
+title('Trajectory');
 hold off;
 
 % Plot x and y position versus time.
 figure(2);
 plot(t, xpos, 'r', t, ypos, 'b-.');
-legend('X', 'Y', 'Location', 'Best');
+legend('\DeltaX', '\DeltaY', 'Location', 'Upper Right');
+title('Position Errors');
 
 % Plot the calculated input controls versus time.
 figure(3);
 plot(t, w, 'r', t, u, 'b-.');
-legend('w', 'u', 'Location', 'Best');
+legend('w', 'u', 'Location', 'Upper Right');
+title('Velocities');
 
 % Plot the derivative of the Lyapunov function versus time. This should be
 % negative.
 figure(4);
 plot(t, Vdot, 'r');
-legend('Vdot', 'Location', 'Best');
+legend('Vdot', 'Location', 'Upper Right');
+title('Vdot');
