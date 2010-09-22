d61444d3214740e1aaa5abbb74f8c13deb2865ed
tdenewiler <tdenewiler@68414a23-2066-41b6-81c2-f5b33d658ffc>
2010-09-22 07:04:00 +0000

    Converted model based controller code to Python. Trying to learn more about what the gains do in simulation but it doesn't behave exactly the same... think it has something to do with yaw angles but need to look into it more.
    Former-commit-id: dc309e28da8032a484f1ce9292091e6de1b73119
    Former-commit-id: 84aaf52c1da4e80d12c03172415f9a57827b7684

--- code/lyapunovRusu/main.m
+++ code/lyapunovRusu/main.m
@@ -2,25 +2,22 @@
 clc; clf; clear;
 
 % Declare variables.
-xstart = 20;
-ystart = -10;
+xstart = 1;
+ystart = -1;
 yawstart = 0;
 xend = 0;
 yend = 0;
 yawend = 0;
-step = 0.2;
-time = 30;
-maxu = 5;
-maxw = 5;
-borderpercent = 1.1;
+time = 10;
+plotpad = 1.1;
 
-% Parameters.
+% Gains.
 % Note that the gamma, h and k parameters are passed to f3 below as part
 % of y only so that they don't have to be set again in the f3 function
 % and can be modified here only.
-gamma = 0.3;
+gamma = 0.24;
 h = 0.1;
-k = 1;
+k = 0.25;
 
 % Calculate the initial errors for position and heading.
 [e, theta, a] = CalcETA(xstart-xend, ystart-yend, yawstart-yawend);
@@ -36,48 +33,30 @@ ypos = zeros(1,length(t));
 w = zeros(1,length(t));
 u = zeros(1,length(t));
 Vdot = zeros(1,length(t));
-xposmin = 0;
-xposmax = 0;
-yposmin = 0;
-yposmax = 0;
 
-% At each time step calculate the forward velocity and angular rate commands
-% to issue to the robot. Note that Vdot should be <= 0 is a sanity check.
+% At each time step calculate the linear and angular velocity commands.
+% Note that Vdot should be <= 0 is a sanity check.
 for i = 1:length(t)
     xpos(i) = sqrt(y(i,1)^2 * (cos(y(i,3))^2));
     ypos(i) = xpos(i) * tan(y(i,3));
     u(i) = gamma * cos(y(i,2)) * y(i,1);
-    if abs(u(i)) > maxu
-        u(i) = sign(u(i)) * maxu;
-    end
     w(i) = k * y(i,2) + gamma * (cos(y(i,2)) * ...
         sin(y(i,2)) / y(i,2)) * (y(i,2) + h * y(i,3));
-    if abs(w(i)) > maxw
-        w(i) = sign(w(i)) * maxw;
-    end
     Vdot = -y(i,1) * u * cos(y(i,2)) + y(i,2) * ...
         (-w(i) + u(i) * sin(y(i,2)) / y(i,2) * ...
         (y(i,2) + h * theta) / y(i,1));
-
-    % Set up the axes on the plot to show entire trajectory.
-    if xpos(i) > xposmax
-        xposmax = xpos(i);
-    end
-    if xpos(i) < xposmin
-        xposmin = xpos(i);
-    end
-    if ypos(i) > yposmax
-        yposmax = ypos(i);
-    end
-    if ypos(i) < yposmin
-        yposmin = ypos(i);
-    end
 end
+
+% Find the position limits to use when plotting.
+xposmin = min(xpos);
+xposmax = max(xpos);
+yposmin = min(ypos);
+yposmax = max(ypos);
 
 % Plot the trajectory to go from the start to the goal.
 figure(1);
 hold on;
-axis([borderpercent * xposmin borderpercent * xposmax borderpercent * yposmin borderpercent * yposmax]);
+axis([plotpad*xposmin plotpad*xposmax plotpad*yposmin plotpad*yposmax]);
 axis equal
 plot(xpos, ypos, 'b');
 plot(xstart, ystart, 'go');
@@ -88,18 +67,24 @@ hold off;
 % Plot x and y position versus time.
 figure(2);
 plot(t, xpos, 'r', t, ypos, 'b-.');
-legend('\DeltaX', '\DeltaY', 'Location', 'Upper Right');
+legend('\DeltaX', '\DeltaY');
 title('Position Errors');
 
 % Plot the calculated input controls versus time.
 figure(3);
 plot(t, w, 'r', t, u, 'b-.');
-legend('w', 'u', 'Location', 'Upper Right');
+legend('w', 'u');
 title('Velocities');
+
+% Plot the errors returned by the ODE solver.
+figure(4);
+plot(t, y(:,1), t, y(:,2), t, y(:,3));
+legend('e', '\alpha', '\theta');
+title('Errors');
 
 % Plot the derivative of the Lyapunov function versus time. This should be
 % negative.
-figure(4);
+figure(5);
 plot(t, Vdot, 'r');
-legend('Vdot', 'Location', 'Upper Right');
+legend('Vdot');
 title('Vdot');
