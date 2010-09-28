7bd1686693f5d9daea376799b3bc5687302fc06c
tdenewiler <tdenewiler@68414a23-2066-41b6-81c2-f5b33d658ffc>
2010-09-28 22:00:57 +0000

    Figured out a lot about how the gains should be set up and put that in the Controls chapter although it means that I have notes to myself once again. At least this time all I should need to do is clean up that subsection. Modified and added scripts to help with analysis and selection of gains.
    Former-commit-id: 9c18f6b3860f73908326b88af9717fe0d7e7cd4b
    Former-commit-id: da93121fca1afc16a994ee8e1d2a485ceeeae322

---
+++ code/findEigsLyapunov.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+# Given inputs h and gamma this script will find an
+# appropriate k to ensure that:
+# (i) The linear system created as the angle errors (alpha, theta)
+# approaches (0,0) is critically damped meaning that the
+# two eigenvalues are the same.
+# (ii) The negative of the eigenvalue is greater than
+# gamma meaning that the angle errors will converge before
+# the distance error.
+
+from scipy import * # For eig()
+from numpy import * # For array()
+import sys
+
+if len(sys.argv) != 3:
+    print 'Not the correct input parameters. Usage is ./lyapunovFindEigs.py <h> <gamma>'
+    sys.exit(1)
+
+h = float(sys.argv[1])
+gamma = float(sys.argv[2])
+
+k = 2*gamma*sqrt(h)
+print 'k =', k
+
+A = array([[-1*k, -1*h*gamma],[gamma, 0]])
+print 'A =', A
+V,D = linalg.eig(A)
+zeta = V[0]/V[1]
+print 'V =', V
+sigma = -V[0]
+print 'sigma =', sigma
+
+if zeta == 1:
+    print 'System is critically damped since zeta =', zeta
+
+if zeta.real > 1:
+    print 'System is overdamped since zeta =', zeta
+
+if zeta.real < 1:
+    print 'System is underdamped since zeta =', zeta
