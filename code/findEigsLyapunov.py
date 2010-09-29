34e3942c6af7b725ec20221104a7d322cd27f170
tdenewiler <tdenewiler@68414a23-2066-41b6-81c2-f5b33d658ffc>
2010-09-29 18:17:25 +0000

    Much better explanation of Lyapunov gains and got rid of notes to myself again. I feel much better about the Controls chapter now.

--- code/findEigsLyapunov.py
+++ code/findEigsLyapunov.py
@@ -1,17 +1,14 @@
 #! /usr/bin/env python
 
-# Given inputs h and gamma this script will find an
-# appropriate k to ensure that:
-# (i) The linear system created as the angle errors (alpha, theta)
-# approaches (0,0) is critically damped meaning that the
-# two eigenvalues are the same.
-# (ii) The negative of the eigenvalue is greater than
-# gamma meaning that the angle errors will converge before
-# the distance error.
+# Given inputs h and gamma this script will find an appropriate k to ensure that:
+# (i) The linear system created as the angle errors (alpha, theta) approaches (0,0)
+# is critically damped meaning that the two eigenvalues are the same.
+# (ii) The negative of the eigenvalue is greater than gamma meaning that the angle
+# errors will converge before the distance error.
 
 from scipy import * # For eig()
 from numpy import * # For array()
-import sys
+import sys # For argv
 
 if len(sys.argv) != 3:
     print 'Not the correct input parameters. Usage is ./lyapunovFindEigs.py <h> <gamma>'
@@ -29,7 +26,6 @@ V,D = linalg.eig(A)
 zeta = V[0]/V[1]
 print 'V =', V
 sigma = -V[0]
-print 'sigma =', sigma
 
 if zeta == 1:
     print 'System is critically damped since zeta =', zeta
@@ -39,3 +35,9 @@ if zeta.real > 1:
 
 if zeta.real < 1:
     print 'System is underdamped since zeta =', zeta
+
+if sigma < gamma:
+    print 'Distance converges FASTER than angles since sigma =', sigma, '<', gamma
+
+if sigma > gamma:
+    print 'Distance converges SLOWER than angles since sigma =', sigma, '>', gamma
