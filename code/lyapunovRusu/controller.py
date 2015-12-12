cbe182642705a4a3f5853bad7943bfd19a3827dc
Thomas Denewiler <tdenewiler@gmail.com>
2015-12-11 21:27:19 -0800

    Fixing pylint issues and refactoring code for Lyapunov simulation.

---
+++ code/lyapunovRusu/controller.py
@@ -0,0 +1,184 @@
+#! /usr/bin/env python
+"""
+Simulation of differential drive robot using model-based controller based on
+Lyapunov stability theory.
+"""
+
+import matplotlib
+# For use over SSH.
+matplotlib.use('Qt4Agg')
+
+#import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import odeint # pylint: disable=no-name-in-module
+from math import sqrt, atan2, cos, sin
+import time
+
+PNGRES = 100
+SAVEIMAGES = 0
+
+def kinematics_ode(state, dummy, gamma, h_gain, k_gain):
+    r"""
+    Solves the differential equations from robot kinematics equations to
+    generate a trajectory that the robot will drive.
+
+    ODEs
+    .. math::
+        \dot{e} = -\gamma e \cos^2\alpha
+        \dot{\alpha} = -k\alpha - \gamma h \frac{\cos\alpha\sin\alpha}{\alpha}
+        \dot{\theta} = \gamma\cos\alpha\sin\alpha
+
+    Variables:
+    state(0) = e
+    state(1) = alpha
+    state(2) = theta
+    """
+    edot = -gamma * state[0] * cos(state[1])**2
+    alphadot = -k_gain * state[1] - \
+        gamma * h_gain * state[2] * cos(state[1]) * sin(state[1]) / state[1]
+    thetadot = gamma * cos(state[1]) * sin(state[1])
+
+    return [edot, alphadot, thetadot]
+
+#pylint: disable=too-many-instance-attributes
+class LyapunovController(object):
+#pylint: enable=too-many-instance-attributes
+    """
+    Configuration values for controller.
+    """
+    def __init__(self):
+        self.x_i = 0
+        self.y_i = 0
+        self.yaw_i = 0
+        self.x_f = 0
+        self.y_f = 0
+        self.yaw_f = 0
+        #self.dxe = 0
+        #self.dye = 0
+        #self.thetastar = 0
+        #self.psi = 0
+        #self.e_dist = 0
+        #self.thetae = 0
+        #self.phi = 0
+        #self.theta = 0
+        #self.alpha = 0
+        self.t_range = 0
+        self.start = 0
+        self.x_pos = []
+        self.y_pos = []
+
+    def initialize_states(self, x_i, y_i, yaw_i, x_f, y_f, yaw_f, t_range):
+        """
+        Initialize start and end states.
+        """
+        self.x_i = x_i
+        self.y_i = y_i
+        self.yaw_i = yaw_i
+        self.x_f = x_f
+        self.y_f = y_f
+        self.yaw_f = yaw_f
+        self.t_range = t_range
+
+    def run(self, gamma, h_gain, k_gain):
+        """
+        Run the controller.
+        """
+        state_init = self.calculate_errors()
+        state_final = self.simulate(state_init, gamma, h_gain, k_gain)
+        t_end = 40
+        t_inc = 0.1
+        self.x_pos, self.y_pos = self.calculate_commands(state_final, t_end, \
+            t_inc, gamma, h_gain, k_gain)
+
+    def calculate_errors(self):
+        """
+        Calculate errors associated with controller state.
+        """
+        dxe = self.x_f - self.x_i
+        dye = self.y_f - self.y_i
+        thetastar = self.yaw_f
+        psi = self.yaw_i
+        e_dist = sqrt((dxe)**2 + (dye)**2)
+        thetae = atan2(dye, dxe)
+        phi = thetastar - psi
+        theta = thetae - thetastar
+        alpha = theta - phi
+        state_init = [e_dist, theta, alpha]
+        print 'state_init = '
+        print state_init
+
+        return state_init
+
+    def simulate(self, state_init, gamma, h_gain, k_gain):
+        """
+        Simulate run.
+        """
+        self.start = time.time()
+        state_final = odeint(kinematics_ode, state_init, \
+            self.t_range, (gamma, h_gain, k_gain))
+
+        print 'state_final ='
+        print state_final
+        return state_final
+
+    def calculate_commands(self, state_final, t_end, t_inc, gamma, h_gain, \
+        k_gain):
+        """
+        Calculate commands at each time step.
+        """
+# pylint: disable=no-member
+        x_pos = np.zeros((t_end / t_inc, 1))
+        y_pos = np.zeros((t_end / t_inc, 1))
+        cmd_u = np.zeros((t_end / t_inc, 1))
+        cmd_w = np.zeros((t_end / t_inc, 1))
+# pylint: enable=no-member
+
+        for t_step in range(0, int(t_end / t_inc)):
+            cmd_u[t_step] = gamma * state_final[t_step, 0] * \
+                cos(state_final[t_step, 1])
+            cmd_w[t_step] = k_gain * state_final[t_step, 1] + \
+                gamma * cos(state_final[t_step, 1]) * \
+                sin(state_final[t_step, 1]) / state_final[t_step, 1] * \
+                (state_final[t_step, 1] + h_gain * state_final[t_step, 2])
+            x_pos[t_step] = self.x_f - state_final[t_step, 0] * \
+                cos(state_final[t_step, 2])
+            y_pos[t_step] = self.y_f - state_final[t_step, 0] * \
+                sin(state_final[t_step, 2])
+        t_elapsed = time.time() - self.start
+        print 'Simulation took %0.5f seconds for a rate of %0.2f Hz.' % \
+            (t_elapsed, 1 / t_elapsed)
+
+        return x_pos, y_pos
+
+def main():
+    """
+    Run controllers with different gains.
+    """
+    print 'Creating controller 1'
+    controller = LyapunovController()
+    print 'Initializing controller 1'
+    x_i = 5
+    y_i = -2
+    yaw_i = 0
+    x_f = -15
+    y_f = 13
+    yaw_f = 0
+    t_trange = 40
+    controller.initialize_states(x_i, y_i, yaw_i, x_f, y_f, yaw_f, t_trange)
+    gamma = 0.25
+    h_gain = 1.2
+    k_gain = 2.5
+    print 'Running controller 1'
+    controller.run(gamma, h_gain, k_gain)
+
+    print 'Creating controller 2'
+    print 'Initializing controller 2'
+    controller.initialize_states(x_i, y_i, yaw_i, x_f, y_f, yaw_f, t_trange)
+    gamma = 0.25
+    h_gain = 1.10
+    k_gain = 2 * gamma * sqrt(h_gain)
+    print 'Running controller 2'
+    controller.run(gamma, h_gain, k_gain)
+
+if __name__ == '__main__':
+    main()
